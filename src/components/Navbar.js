32c0dde0696545059b67adadd0229af638f18040
iyiz <23031421@myrp.edu.sg>
2025-01-23 18:50:45 +0800

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // Assume you have some CSS for your navbar
+import logo from '../images/logo.png';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About Me</Link></li>
-                <li><Link to="/portfolio">Portfolio</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
+            <div className="navbar-container">
+                <img src={logo} alt="Logo" className="navbar-logo" />
+                <ul className="navbar-links">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Me</Link></li>
+                    <li><Link to="/portfolio">Portfolio</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </div>
         </nav>
     );
 };
